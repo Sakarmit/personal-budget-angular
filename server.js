9cacdc1a00aa0a7f0485593c87e1be195b79a8df
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-09-14 11:01:24 -0400

    Added server, Axios, and ChartJS * Most things were already present because of personal-budget being cloned instead of personal-budget-1 originally - Removed cors for the time being

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 // Budget API
 
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use('/', express.static('public'));
 
 const budget = {
     myBudget: [
@@ -24,6 +23,9 @@ const budget = {
     ]
 };
 
+app.get('/hello', (req, res) => {
+    res.send('Hello World');
+});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
