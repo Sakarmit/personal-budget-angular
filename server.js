31d7fb08931a3f2e360f8d5c803f247127b3016a
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-09-14 11:05:46 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -6,29 +6,12 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
-
 app.get('/hello', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.sendFile(__dirname + '\\data.json');
 });
 
 app.listen(port, () => {
